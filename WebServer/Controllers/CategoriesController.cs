6ac495cb4364aaa731031b33fe40e777939d07b4
xframee <alexander@kmax.dk>
2023-10-24 18:05:54 +0200

    Updated

--- WebServer/Controllers/CategoriesController.cs
+++ WebServer/Controllers/CategoriesController.cs
@@ -1,12 +1,45 @@
+using System.Collections;
+using DataLayer;
 using Microsoft.AspNetCore.Mvc;
+using WebServer.Models;
 
 namespace WebServer.Controllers;
 
 [Route("api/categories")]
+[ApiController]
 public class CategoriesController : ControllerBase
 {
-    public string SayHello()
+    DataService ds = new DataService();
+        
+    [HttpGet]
+    public IActionResult GetCategories()
     {
-        return "Hello from ASP";
+        return Ok(ds.GetCategories());
     }
+
+    [HttpGet("{id}")]
+    public IActionResult GetCategory(int id)
+    {
+        var category = ds.GetCategory((id));
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public IActionResult CreateCategory(CreateCategoryModel model)
+    {
+        var category = new Category
+        {
+            Name = model.Name,
+            Description = model.Description
+        };
+        
+        ds.CreateCategory(category);
+        return Ok(category);
+    }
+    
 }
